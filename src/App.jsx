77d899fd7ef65d4048bacfda5c1128e06787720d
TruongTanNghia <tannghiaxp@gmail.com>
2023-09-26 15:29:37 +0700

    #1 upadate Setup file

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import { useState } from 'react';
 import './App.css';
-
 function App() {
   const [count, setCount] = useState(0);
 
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,24 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import viteCompression from 'vite-plugin-compression';
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    viteCompression({
+      verbose: false,
+      algorithm: 'gzip',
+      ext: '.gz',
+    }),
+  ],
+  resolve: {
+    alias: {
+      '@': 'src',
+      '@Image': '/src/assets',
+      '@Component': 'src/component',
+      '@Imports': 'src/imports/*',
+      '@Styles': 'src/styles',
+      '@Pages': 'src/pages',
+    },
+  },
+});
