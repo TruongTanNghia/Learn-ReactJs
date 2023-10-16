d96f032a0dba41bae6ea3d5f9530946ad8b02b26
TruongTanNghia <tannghiaxp@gmail.com>
2023-10-16 17:55:54 +0700

    #7 Update Navbar

--- src/layouts/navBar/index.jsx
+++ src/layouts/navBar/index.jsx
@@ -1,5 +1,47 @@
+import NavlinkActive from '@/routers/navlinkActive';
+
+import './navbar.css';
 const NavBar = () => {
-  return <div>---------NavBar------</div>;
+  return (
+    <>
+      <ul className="display">
+        <li className="thefirst">
+          <NavlinkActive to=""> Home </NavlinkActive>
+        </li>
+        <li className="thefirst">
+          <NavlinkActive to="useState"> useState</NavlinkActive>
+          <ul className="sub-menu2">
+            <li>
+              <NavlinkActive to="useState/bai1useState">Exercise 1</NavlinkActive>
+            </li>
+            <li>
+              <NavlinkActive to="useState/bai2useState">Exercise 2</NavlinkActive>
+            </li>
+            <li>
+              <NavlinkActive to="useState/bai3useState">Exercise 3</NavlinkActive>
+            </li>
+          </ul>
+        </li>
+        <li className="thefirst">
+          <NavlinkActive to="useEffect"> useEffect</NavlinkActive>
+          <ul className="sub-menu2">
+            <li>
+              <NavlinkActive to="useEffect/bai1useEffect">Exercise 1</NavlinkActive>
+            </li>
+            <li>
+              <NavlinkActive to="useEffect/bai2useEffect">Exercise 2</NavlinkActive>
+            </li>
+            <li>
+              <NavlinkActive to="useEffect/bai4useEffect">Exercise 4</NavlinkActive>
+            </li>
+          </ul>
+        </li>
+        <li className="thefirst">
+          <NavlinkActive to="projects/projects1">TodoList</NavlinkActive>
+        </li>
+      </ul>
+    </>
+  );
 };
 
 export default NavBar;
