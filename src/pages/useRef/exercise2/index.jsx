71ff9ec067b2faadcb42e4e397bb69ee5048aea0
TruongTanNghia <tannghiaxp@gmail.com>
2023-10-19 21:57:57 +0700

    #14 Exercise 2 useRef

--- src/pages/useRef/exercise2/index.jsx
+++ src/pages/useRef/exercise2/index.jsx
@@ -1,5 +1,31 @@
-const Bai2UseRef = () => {
-  return <div>Bai2UseRef</div>;
-};
+import { useEffect, useRef, useState } from 'react';
 
+const Bai2UseRef = () => {
+  const [count, setCount] = useState(0);
+  const prevIdCount = useRef(count);
+  const stopCount = useRef();
+
+  const handleCounter = () => {
+    stopCount.current = setInterval(() => {
+      setCount((prev) => prev + 1);
+    }, 1000);
+  };
+
+  const handleStop = () => {
+    clearInterval(stopCount.current);
+  };
+  useEffect(() => {
+    prevIdCount.current = count;
+  }, [count]);
+
+  // console.log(prevIdCount.current, 'nó là cái quầN què gì ?');
+  return (
+    <>
+      <h1>New: {count}</h1>
+      <h1>Old: {prevIdCount.current}</h1>
+      <button onClick={handleCounter}>Start</button>
+      <button onClick={handleStop}>Stop</button>
+    </>
+  );
+};
 export default Bai2UseRef;
