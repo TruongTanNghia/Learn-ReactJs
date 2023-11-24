8bbb640463bf45d59d20339df0f3f063d321c2cb
TruongTanNghia <tannghiaxp@gmail.com>
2023-11-24 12:24:54 +0700

    #21 feat: Page-Notfound

--- src/pages/notfound/index.jsx
+++ src/pages/notfound/index.jsx
@@ -1,5 +1,15 @@
+/* eslint-disable react/no-unescaped-entities */
+import './style.css';
 const NotfoundPage = () => {
-  return <div>NotfoundPage</div>;
+  return (
+    <>
+      <title>Page Not Found</title>
+      <img className="container" src="https://i.ibb.co/W6tgcKQ/softcodeon.gif" />
+      <h1 className="error-text">Whoops, We can't seem to find the resource you're looking for.</h1>
+      <p className="text">Please check that the Web site address is spelled correctly.Or,</p>
+      <div className="btn1"></div>
+    </>
+  );
 };
 
 export default NotfoundPage;
